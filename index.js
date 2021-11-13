213f4a79720d53fef5fe1bbfdb2e2da0b91ce80c
Max <ma15fo43@gmail.com>
2021-11-13 23:35:27 +0100

    🦋 fix: HTTP server not listening

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import http from "http";
 import https from "https";
 import fs from "fs";
 
@@ -7,7 +8,7 @@ import * as logger from "./utils/logger.js";
 const app = express();
 const port = 443;
 
-registerServer();
+registerServers();
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -17,6 +18,10 @@ const urls = {
     mango: "https://discord.com/api/oauth2/authorize?client_id=497443144632238090&permissions=268758135&scope=bot%20applications.commands",
     self: "https://github.com/fowled/go"
 };
+
+http.get("*", async (req, res) => {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+});
 
 app.get("/", async (req, res) => {
     return res.redirect(urls.self);
@@ -31,6 +36,10 @@ app.get("/:url/", async (req, res) => {
 });
 
 function registerServer() {
+    const httpServer = http.createServer(app).listen(80).on("listening", () => {
+        logger.log("HTTP server listening on port 80");
+    });
+    
     const httpsServer = https.createServer({
         key: fs.readFileSync('/etc/letsencrypt/live/go.fowled.club/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/go.fowled.club/fullchain.pem'),
